7a067d59ce76e077f62b5ae988ab6e3945772ad3
t-maru078 <13112828+t-maru078@users.noreply.github.com>
2022-12-13 10:12:28 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # code-pipeline-slack-notification
+
+CodePipeline で発生するイベントを Slack に通知するサンプルです。
+
+CloudFormation template を使って AWS リソースを定義しているため必要な環境をコマンド 1 つでデプロイ可能です。
+
+今回は CodePipeline のイベントを通知するように設定していますが、通知対象の Event ID を変更することで CodeBuild など他のイベント通知にも利用できます。通知で設定できるイベントの詳細は下記の公式ドキュメントを参照してください。
+
+https://docs.aws.amazon.com/dtconsole/latest/userguide/concepts.html#concepts-api
+
+
+## CI 部分の構成図
+
+![workflow](./docs/assets/workflow.jpg)
+
+
+## AWS へのデプロイ
+
+事前に下記の作業 3 つが必要です。
+
+1. AWS CLI のインストール。詳細は下記の公式ドキュメント参照。
+
+    https://docs.aws.amazon.com/ja_jp/cli/latest/userguide/getting-started-install.html
+
+1. GitHub にて Personal access token を取得する。詳細は下記の公式ドキュメント参照。
+
+    https://docs.aws.amazon.com/ja_jp/codebuild/latest/userguide/access-tokens.html
+
+1. 上記で取得した token をデプロイコマンド実行前に AWS の Management Console で設定する
+
+    1. CodeBuild の console を開き `Create build project` ボタンを押す
+    1. Source provider のドロップダウンから `GitHub` を選択する
+    1. `Connect with a GitHub personal access token` を選択し、`GitHub personal access token` の欄に GitHub から取得した Personal access token を入力し `Save token` ボタンを押す
+    1. console 最下部の `cancel` ボタンを押して `Create build project` のウィザードを閉じる
+
+1. AWS Chatbot コンソールを開き、Slack workspace との接続を行う
+
+    1. `Configure a chat client` のセクションで Chat client から　Slack を選択し、`Configure client` を押す
+    1. 、Slack の設定画面が開き AWS からのアクセスを許可するかどうかを尋ねられるので、内容を確認し問題なければ Allow をクリックする
+
+1. `scripts/env.template` をコピーして `scripts/.env` ファイルを作成し、必要なパラメータを記載する
+
+    | Parameter name | Description | Required |
+    |--|--|--|
+    | TEMPLATE_STORE_S3_BUCKET_NAME | CloudFormation template を格納する S3 バケット。<br />ここで指定した S3 に対してデプロイスクリプトが必要な template を動的にアップロードします。 | Yes |
+    | GITHUB_REPOSITORY_URL | Pipeline をトリガーする GitHub リポジトリの URL | Yes |
+    | SLACK_WORKSPACE_ID | 通知対象の Channel がある Slack workspace の ID | Yes |
+    | SLACK_CHANNEL_ID | 通知対象の Channel の ID | Yes |
+    | AWS_PROFILE | AWS CLI 実行時に使用する AWS の Profile。<br />指定しない場合は default profile が使用されます。 | No |
+
+上記の手順が完了後、この README と同じディレクトリ階層で下記コマンドを実行することで必要な環境が AWS 上にデプロイされます。
+
+```
+bash scripts/deploy-pipeline.sh
+```
+
+## 注意事項
+
+- このサンプルは GitHub Apps などを設定する必要がなく上記のコマンドを使って AWS リソースをデプロイするだけで使用可能になりますが、GitHub の Webhook により起動される CodeBuild の Container は Pipeline 処理が完了か timeout するまで起動し続けますので、ビルドやテストなどに時間がかかるアプリケーションの場合 CodeBuild の料金がかさむ可能性がありますのでご注意ください。
+
+- CloudFormation stack の削除時に不要な S3 リソースが残らないように DeletionPolicy は設定しておりません。Pipeline 実行後は S3 内部にファイルが作成されておりますので stack の削除時に DELETE_FAILED 状態になりますが S3 内部のファイルを削除 (Object のすべての version を削除) してから再度 stack の削除を実行すると正常に削除されます。
---
+++ scripts/deploy-pipeline.sh
@@ -0,0 +1,147 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd "$(dirname $0)" || exit; pwd)
+eval "$(cat ${SCRIPT_DIR}/.env <(echo) <(declare -x))"
+
+: ==================================================
+:  Constants
+: ==================================================
+PROJECT_NAME="github-webhook-pipeline"
+S3_BUCKET_NAME=${TEMPLATE_STORE_S3_BUCKET_NAME:-$$-cfn-templates-store}
+S3_PATH="s3://${TEMPLATE_STORE_S3_BUCKET_NAME}/${PROJECT_NAME}"
+EXPIRE_DURATION=300
+PIPELINE_TMPL_LIST="
+ci-resources.yml
+pipeline-resources/source-code-repository.yml
+pipeline-resources/artifact-repository.yml
+pipeline-resources/unit-tests-project.yml
+pipeline-resources/pipeline.yml
+pipeline-resources/github-webhook-project.yml
+notification-resources/chatbot.yml
+notification-resources/notifications.yml
+"
+[ -n "${AWS_PROFILE}" ] && AWS_CLI_OPTIONS="--profile ${AWS_PROFILE}"
+
+: ==================================================
+:  Functions
+: ==================================================
+function createS3Bucket() {
+  local readonly s3BucketName=$1
+
+  echo -e "    creating..."
+  RESULT=$(aws ${AWS_CLI_OPTIONS} s3 mb s3://${s3BucketName}/ 2>&1)
+  if [ $? -eq 0 ]; then
+    echo -e "    The S3 Bucket(${s3BucketName}) successfully created.\n"
+  else
+    echo $RESULT | grep "BucketAlreadyOwnedByYou" 1>/dev/null
+    if [ $? -eq 0 ]; then
+      echo -e "    Using the existing S3 bucket(${s3BucketName}).\n"
+    else
+      echo -e "    The requested S3 bucket name(${s3BucketName}) is not available. Please check a name and try again!\n"
+      exit 1
+    fi
+  fi
+}
+
+function validate() {
+  error_flag=false
+
+  for template in $(cat)
+  do
+    error=$(aws ${AWS_CLI_OPTIONS} cloudformation validate-template --template-body file://./ci/${template} 2>&1 > /dev/null)
+    if [ $? -eq 0 ]
+    then
+      echo -e "      ${template} ----- \u001b[32m✔︎\u001b[0m"
+    else
+      error_flag=true
+      echo -e "      ${template} ----- \u001b[31m✖︎\u001b[0m"
+      echo -e "\u001b[31m${error}\u001b[0m\n"
+    fi
+  done
+
+  if ${error_flag}
+  then
+    exit 1
+  fi
+}
+
+function generateSignedUrl() {
+  for TMPL in $(cat)
+  do
+    echo $(aws ${AWS_CLI_OPTIONS} s3 presign ${S3_PATH}/${PROJECT_NAME}/${TMPL} --expires-in $EXPIRE_DURATION)
+  done
+}
+
+function createStack() {
+  local readonly STACK_NAME=$1
+  local readonly TEMPLATE=$2
+  local readonly PARAMETERS=$3
+
+  aws ${AWS_CLI_OPTIONS} \
+    cloudformation create-stack \
+    --stack-name ${STACK_NAME} \
+    --template-url ${TEMPLATE} \
+    --parameters ${PARAMETERS} \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --on-failure DELETE
+  aws ${AWS_CLI_OPTIONS} cloudformation wait stack-create-complete --stack-name ${STACK_NAME}
+}
+
+function updateStack() {
+  local readonly STACK_NAME=$1
+  local readonly TEMPLATE=$2
+  local readonly PARAMETERS=$3
+
+  aws ${AWS_CLI_OPTIONS} \
+    cloudformation update-stack \
+    --stack-name ${STACK_NAME} \
+    --template-url ${TEMPLATE} \
+    --parameters ${3} \
+    --capabilities CAPABILITY_NAMED_IAM
+  aws ${AWS_CLI_OPTIONS} cloudformation wait stack-update-complete --stack-name ${STACK_NAME}
+}
+
+: ==================================================
+:  Main
+: ==================================================
+echo -e "\n  deploy pipeline starting...\n"
+echo -e "    [1] Create S3 Bucket\n"
+createS3Bucket ${TEMPLATE_STORE_S3_BUCKET_NAME}
+[ $? -ne 0 ] && exit 1
+
+echo -e "    [2] validate CFn templates \n"
+echo ${PIPELINE_TMPL_LIST} | validate
+[ $? -ne 0 ] && exit 1
+
+echo -e "\n    [3] upload to S3 bucket \n"
+aws ${AWS_CLI_OPTIONS} s3 sync ./ci ${S3_PATH}/${PROJECT_NAME} --exclude "codebuild/**/*"
+
+echo -e "\n    [4] generate signed urls \n"
+PIPELINE_URLS=($(echo ${PIPELINE_TMPL_LIST} | generateSignedUrl))
+
+echo -e "\n    [5] deploy stack of pipeline\n"
+PIPELINE_STACK_NAME="pipeline-notification"
+PARAMETERS_FILE="/tmp/$$-parameter.json"
+PIPELINE_PARAMETERS="[
+{\"ParameterKey\":\"SourceCodeRepositoryTmplPath\",\"ParameterValue\":\"${PIPELINE_URLS[1]}\"},
+{\"ParameterKey\":\"ArtifactRepositoryTmplPath\",\"ParameterValue\":\"${PIPELINE_URLS[2]}\"},
+{\"ParameterKey\":\"UnitTestsProjectTmplPath\",\"ParameterValue\":\"${PIPELINE_URLS[3]}\"},
+{\"ParameterKey\":\"PipelineTmplPath\",\"ParameterValue\":\"${PIPELINE_URLS[4]}\"},
+{\"ParameterKey\":\"GithubWebhookProjectTmplPath\",\"ParameterValue\":\"${PIPELINE_URLS[5]}\"},
+{\"ParameterKey\":\"ChatBotTmplPath\",\"ParameterValue\":\"${PIPELINE_URLS[6]}\"},
+{\"ParameterKey\":\"NotificationsTmplPath\",\"ParameterValue\":\"${PIPELINE_URLS[7]}\"},
+{\"ParameterKey\":\"SourceCodeRepositoryURL\",\"ParameterValue\":\"${GITHUB_REPOSITORY_URL}\"},
+{\"ParameterKey\":\"SlackWorkspaceId\",\"ParameterValue\":\"${SLACK_WORKSPACE_ID}\"},
+{\"ParameterKey\":\"SlackChannelId\",\"ParameterValue\":\"${SLACK_CHANNEL_ID}\"}
+]"
+echo ${PIPELINE_PARAMETERS} > ${PARAMETERS_FILE}
+
+echo -e "\n      pipeline deploy start."
+error=$(aws ${AWS_CLI_OPTIONS} cloudformation describe-stacks --stack-name "${PIPELINE_STACK_NAME}" 2>&1 > /dev/null)
+if [ $? -ne 0 ]; then
+  createStack ${PIPELINE_STACK_NAME} ${PIPELINE_URLS[0]} "file://${PARAMETERS_FILE}"
+else
+  updateStack ${PIPELINE_STACK_NAME} ${PIPELINE_URLS[0]} "file://${PARAMETERS_FILE}"
+fi
+
+rm ${PARAMETERS_FILE}
